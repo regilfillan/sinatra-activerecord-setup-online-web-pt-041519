4ae1b7e5412114a0ec6fe96f7f700192f2a35555
Ryan Gilfillan <regilfillan@gmail.com>
2019-08-05 17:04:07 +0000

    Automatically backed up by Learn

--- db/migrate/20190805165944_create_dogs.rb
+++ db/migrate/20190805165944_create_dogs.rb
@@ -7,15 +7,15 @@ class CreateDogs < ActiveRecord::Migration[5.2]
     end
   end
   
-  def up
-    create_table :dogs do |t|
-      t.string :name 
-      t.string :breed 
-    end
-  end
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name 
+  #     t.string :breed 
+  #   end
+  # end
   
-  def down
-    drop_table :dogs
-  end
+  # def down
+  #   drop_table :dogs
+  # end
 
 end
